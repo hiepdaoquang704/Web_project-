993c564e1ccea38e10b30c95ac923439182fda3c
hiepdaoquang704 <hiepdaoquang704@gmail.com>
2024-06-19 00:43:51 +0700

    remove test

--- test.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-    var content = document.getElementById('content');
-    if (content.classList.contains('show')) {
-        content.classList.remove('show');
-    } else {
-        content.classList.add('show');
-    }
-});
